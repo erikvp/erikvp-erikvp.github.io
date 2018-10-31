996509bfb63eb3bea2e4cffb1708f07e708dbfb1
erikvp <erikvp@yahoo.com>
2018-10-30 21:26:32 -0400

    updated to two lines with audio & p5 lib file scripts

--- main.js
+++ main.js
@@ -1,63 +1,94 @@
-console.log("sandbox-p5");
+console.log("p5 sandbox");
+let osc1, osc2;
+let playing = false;
+// let width = 800;
 
-let lines = [];
-let numLines = 6;
+// let height = 800;
+let y = 400;
+let angle = 1;
+let timer = 0;
 
-setup = () => {
-  // let canvas = createCanvas(960, 540);
-  let canvas = createCanvas(windowWidth, windowHeight);
-  canvas.parent("canvas-container");
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // createCanvas(width, height);
+  backgroundColor = 20;
+  textAlign(CENTER);
 
-  // Line(y, yInc, angle)
-  for (let i = 0; i < numLines; i++) {
-    let y = random(-70, 0); // start position at top of screen
-    let yInc = random(0.2, 1.0); // line speed
-    let angle = random(0.0, 2); //start angle for color change
-    let x1 = width / 2 - 100; // center of screen - 100
-    let x2 = x1 + 200;
-    lines.push(new Line(x1, x2, y, yInc, angle));
-  }
-};
+  osc1 = new p5.Oscillator();
+  delay1 = new p5.Delay();
+  osc1.setType("sine");
+  osc1.freq(200);
+  // osc1.amp(0);
+  osc1.amp(0.5, 0.05);
+  osc1.start();
+  // source, delayTime, feedback, filter frequency
+  delay1.process(osc1, 0.9, 0.4, 1800);
 
-draw = () => {
-  background(0); // Set the background to black
-  for (let i = 0; i < numLines; i++) {
-    lines[i].move();
-    lines[i].changeColor();
-  }
-};
-
-class Line {
-  constructor(x1, x2, y, yInc, angle) {
-    this.x1 = x1;
-    this.x2 = x2;
-    this.y = y;
-    this.yInc = yInc;
-    this.color = 255; //whiteit
-    this.angle = angle; //controls sine for color change
-  }
-  move() {
-    //when line scrolls off bottom of screen, start at the top
-    this.y <= height ? (this.y += this.yInc) : (this.y = random(-50, 0));
-  }
-
-  changeColor() {
-    let c = 0;
-    let a = 0.0;
-    let inc = TWO_PI / 360;
-
-    this.angle <= TWO_PI ? (this.angle += inc) : (this.angle = 0);
-    this.color = sin(this.angle);
-
-    if (Math.abs(lines[0].y - lines[5].y) < 5) {
-      stroke(10, 230, 22);
-      line(this.x1, this.y, this.x2, this.y);
-    } else {
-      c = Math.round(map(this.color, -1, 1, 30, 255));
-      stroke(c);
-      strokeWeight(1);
-      //line (x1, y1, x2, y2)
-      line(this.x1, this.y, this.x2, this.y);
-    }
-  }
+  osc2 = new p5.Oscillator();
+  delay2 = new p5.Delay();
+  osc2.setType("sine");
+  osc2.freq(200);
+  // osc2.amp(0);
+  osc2.amp(0.5, 0.05);
+  osc2.start();
+  // source, delayTime, feedback, filter frequency
+  delay2.process(osc2, 0.9, 0.4, 1800);
 }
+
+function draw() {
+  background(backgroundColor);
+  // text("click to play", width / 2, height / 2);
+  drawLines();
+  setFreq();
+  checkTimer();
+}
+
+// function mouseClicked() {
+//   if (!playing) {
+//     // ramp amplitude to 0.5 over 0.05 seconds
+//     osc1.amp(0.5, 0.05);
+//     osc2.amp(0.5, 0.05);
+//     playing = true;
+//     backgroundColor = color(10, 10, 10);
+//   } else {
+//     // ramp amplitude to 0 over 0.5 seconds
+//     osc1.amp(0, 0.5);
+//     osc2.amp(0, 0.5);
+//     playing = false;
+//     backgroundColor = color(200, 10, 25);
+//   }
+// }
+
+function setFreq() {
+  let freq1 = 200;
+  let freq2 = 200;
+
+  // freq1 = Math.round(map(mouseY, 0, height, 240, 100));
+  // osc1.freq(freq1);
+
+  freq2 = Math.round(
+    map(y, windowHeight / 2 - 10, windowHeight / 2 + 10, 190, 210)
+  );
+  osc2.freq(freq2);
+
+  console.log(`Freq 1: ${freq1} Freq 2: ${freq2}`);
+}
+
+function drawLines() {
+  y = windowHeight / 2;
+
+  stroke(255);
+  line(0, windowHeight / 2, windowWidth, windowHeight / 2);
+
+  y = sin(angle) * 10 + windowHeight / 2;
+  if (angle > 360) {
+    angle = 0;
+  } else {
+    angle += 0.001;
+    console.log(`Angle: ${angle}`);
+  }
+
+  line(0, y, width, y);
+}
+
+function checkTimer() {}
