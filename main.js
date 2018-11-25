61715b270a417d0a77541e8b22addfc6398a5616
erikvp <erikvp@yahoo.com>
2018-11-25 14:01:21 -0500

    Revised SCREAMING to SCREAM

--- main.js
+++ main.js
@@ -116,6 +116,6 @@ function renderText() {
   textSize(fontSize);
   textFont("VT323");
   text(" A bright green C0W", line1x, line1y);
-  text("Listen to it SCREAMING", line2x, line2y);
+  text("Listen to it SCREAM", line2x, line2y);
   text("I'm hungry for SPINACH", line3x, line3y);
 }
